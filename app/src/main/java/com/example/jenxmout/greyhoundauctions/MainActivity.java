a65ce499e706a8171f9f5f923f0b2820527697d6
kidc12 <kidc12@users.noreply.github.com>
2019-10-10 00:26:59 -0400

    update from von von hamburger menu started but not working

--- app/src/main/java/com/example/jenxmout/greyhoundauctions/MainActivity.java
+++ app/src/main/java/com/example/jenxmout/greyhoundauctions/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.jenxmout.greyhoundauctions;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
