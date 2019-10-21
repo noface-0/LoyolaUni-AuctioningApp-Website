bf010c16809311ec41e2a02a8fd1fe0b07212b0e
jenmoutenot <jenmoutenot4@gmail.com>
2019-10-21 19:39:58 -0400

    adding buttons to activity_sign_up.xml

--- AndroidApp/app/src/main/java/com/example/jenxmout/greyhoundauctions/AccountActivity.java
+++ AndroidApp/app/src/main/java/com/example/jenxmout/greyhoundauctions/AccountActivity.java
@@ -1,39 +1,24 @@
 package com.example.jenxmout.greyhoundauctions;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-
-/**
- * This is the Main Activity class that
- * displays the items up for auction
- * and allows users to search by tags,
- * select items, and access other menu options
- *
- * @author Jennifer Moutenot
- * @author Mollie Morrow
- * @author Ian Leppo
- * @author Javon Kitson
- * @version 1.0 10/21/19
- */
-public class MainActivity extends AppCompatActivity {
+public class AccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
+
 
         Button eventButton = (Button) findViewById(R.id.eventButton);
         eventButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent eventIntent = new Intent(MainActivity.this, EventActivity.class);
+                Intent eventIntent = new Intent(AccountActivity.this, EventActivity.class);
                 startActivity(eventIntent);
             }
         });
@@ -42,10 +27,12 @@ public class MainActivity extends AppCompatActivity {
         accountButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent accountIntent = new Intent(MainActivity.this, AccountActivity.class);
+                Intent accountIntent = new Intent(AccountActivity.this, AccountActivity.class);
                 startActivity(accountIntent);
             }
         });
 
+
     }
+
 }
